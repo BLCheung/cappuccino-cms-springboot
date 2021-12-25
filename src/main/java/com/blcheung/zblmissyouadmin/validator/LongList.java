0576a9c9b2ffc7e019e59c83f64d06bef137c103
BLCheung <blcheung@qq.com>
2021-12-25 23:54:59 +0800

    增加自定义校验注解；

---
+++ src/main/java/com/blcheung/zblmissyouadmin/validator/LongList.java
@@ -0,0 +1,39 @@
+package com.blcheung.zblmissyouadmin.validator;
+
+import com.blcheung.zblmissyouadmin.validator.impl.LongListValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+/**
+ * 校验整型数组
+ *
+ * @author BLCheung
+ * @date 2021/12/11 1:06 上午
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD })
+@Constraint(validatedBy = { LongListValidator.class })
+public @interface LongList {
+
+    long min() default 1;
+
+    long max() default Long.MAX_VALUE;
+
+    /**
+     * 是否可为null或长度为0
+     *
+     * @return boolean
+     * @author BLCheung
+     * @date 2021/12/11 1:13 上午
+     */
+    boolean allowBlank() default false;
+
+    String message() default "{validator.long_list}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
