74678634ade80f1e8b718487ec49451f0d56d70a
BLCheung <blcheung@qq.com>
2022-01-13 06:08:43 +0800

    注入权限拦截器实体；

--- src/main/java/com/blcheung/zblmissyouadmin/common/configuration/WebConfiguration.java
+++ src/main/java/com/blcheung/zblmissyouadmin/common/configuration/WebConfiguration.java
@@ -1,13 +1,21 @@
 package com.blcheung.zblmissyouadmin.common.configuration;
 
+import com.blcheung.zblmissyouadmin.common.interceptor.AuthorizeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author BLCheung
  * @date 2021/12/1 10:45 下午
  */
+@Configuration(proxyBeanMethods = false)
 public class WebConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthorizeInterceptor authorizeInterceptor;
 
     /**
      * 跨域
@@ -19,12 +27,15 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .allowedMethods("*")
                 .maxAge(3600);
     }
 
-
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(this.authorizeInterceptor);
+    }
 }
