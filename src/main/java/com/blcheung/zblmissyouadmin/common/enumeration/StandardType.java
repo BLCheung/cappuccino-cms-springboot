15055b39216052a753b77fac6ca8f472a9b02afc
BLCheung <blcheung@qq.com>
2022-02-18 02:15:02 +0800

    新增规格名与规格值基本模块；

---
+++ src/main/java/com/blcheung/zblmissyouadmin/common/enumeration/StandardType.java
@@ -0,0 +1,23 @@
+package com.blcheung.zblmissyouadmin.common.enumeration;
+
+import com.baomidou.mybatisplus.annotation.IEnum;
+
+/**
+ * @author BLCheung
+ * @date 2022/2/16 8:19 下午
+ */
+public enum StandardType implements IEnum<Integer> {
+    STANDARD(1, "标准类型"),
+    NON_STANDARD(0, "非标准类型");
+
+    private final Integer value;
+
+    StandardType(Integer value, String desc) {
+        this.value = value;
+    }
+
+    @Override
+    public Integer getValue() {
+        return this.value;
+    }
+}
