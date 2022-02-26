9e609dc302fadf4bf8e61430cee9a0a8e2e33463
BLCheung <blcheung@qq.com>
2022-02-26 16:39:57 +0800

    新增MP相关插件配置；

--- src/main/java/com/blcheung/zblmissyouadmin/common/configuration/CommonConfiguration.java
+++ src/main/java/com/blcheung/zblmissyouadmin/common/configuration/CommonConfiguration.java
@@ -2,7 +2,10 @@ package com.blcheung.zblmissyouadmin.common.configuration;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.blcheung.zblmissyouadmin.common.bean.PermissionMetaCollector;
 import com.blcheung.zblmissyouadmin.common.interceptor.AuthorizeInterceptor;
@@ -27,9 +30,15 @@ public class CommonConfiguration {
     }
 
     @Bean
+    public ISqlInjector sqlInjector() { return new DefaultSqlInjector(); }
+
+    @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 分页插件
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 乐观锁插件
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return interceptor;
     }
 }
